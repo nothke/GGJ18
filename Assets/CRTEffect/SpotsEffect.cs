732ea342cf9c760c49167189114ce4fec157c082
Ivan Notaros <nothke@gmail.com>
2018-01-26 19:17:18 +0100

    Added a test scene and debugging spots shader

---
+++ Assets/CRTEffect/SpotsEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotsEffect : EffectBase {
+
+    public Texture2D texture;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //SetCurves();
+
+        material.SetTexture("_MainTexture", texture);
+        //material.SetFloat("_Intensity", intensity);
+        //material.SetFloat("_SourceIntensity", sourceIntensity);
+
+        Graphics.Blit(source, destination, material);
+    }
+}
