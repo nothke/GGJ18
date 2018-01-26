f1ac1561eeb199d5de84fd2a95ea488960834b1a
Ivan Notaros <nothke@gmail.com>
2018-01-26 23:27:43 +0100

    TVParams are now an array

--- Assets/Scripts/TVParams.cs
+++ Assets/Scripts/TVParams.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TVParams : MonoBehaviour
 {
 
-    [Range(0, 1)]
+    [System.Obsolete("Use SetParameter(i, parameter) instead, fuck you Samuli (Horatiu forced me to write this)")]
     public float scrollSpeed;
-    [Range(0, 1)]
+    [System.Obsolete("Use SetParameter(i, parameter) instead")]
     public float snowBandsThreshold;
-    [Range(0, 1)]
+    [System.Obsolete("Use SetParameter(i, parameter) instead")]
     public float crtDistance;
-    [Range(0, 1)]
+    [System.Obsolete("Use SetParameter(i, parameter) instead")]
     public float spotsSize;
 
     CRTEffect _crtEffect;
@@ -20,11 +20,36 @@ public class TVParams : MonoBehaviour
     SpotsEffect _spotsEffect;
     SpotsEffect spotsEffect { get { if (!_spotsEffect) _spotsEffect = GetComponent<SpotsEffect>(); return _spotsEffect; } }
 
+    [SerializeField]
+    float[] testParameters;
+
+    private void Start()
+    {
+        testParameters = new float[4];
+    }
+
+    public void SetParameter(int i, float parameter)
+    {
+        switch (i)
+        {
+            case 0: spotsEffect.scrollSpeed = parameter; break;
+            case 1: crtEffect.distance = Mathf.Lerp(0.01f, 0.6f, parameter); break;
+            case 2: spotsEffect.linesThreshold = 1 - parameter; break;
+            case 3: spotsEffect.spotsSize = parameter; break;
+            default: Debug.Log("YOu have more controllers than parameters"); break;
+        }
+    }
+
+    public bool test;
+
     void Update()
     {
-        spotsEffect.scrollSpeed = scrollSpeed;
-        crtEffect.distance = Mathf.Lerp(0.01f, 0.6f, crtDistance);
-        spotsEffect.linesThreshold = 1 - snowBandsThreshold;
-        spotsEffect.spotsSize = spotsSize;
+        if (test)
+        {
+            for (int i = 0; i < testParameters.Length; i++)
+            {
+                SetParameter(i, testParameters[i]);
+            }
+        }
     }
 }
