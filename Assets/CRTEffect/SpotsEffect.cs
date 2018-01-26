5dc6e9f76a563201bd5aa44e88820209b0158b98
Ivan Notaros <nothke@gmail.com>
2018-01-26 19:38:40 +0100

    Ivan's spots fixed

--- Assets/CRTEffect/SpotsEffect.cs
+++ Assets/CRTEffect/SpotsEffect.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpotsEffect : EffectBase {
+public class SpotsEffect : EffectBase
+{
 
     public Texture2D texture;
+    public float noiseThreshold;
+    [Range(0, 1)]
+    public float intensity = 0.5f;
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        //SetCurves();
-
         material.SetTexture("_MainTexture", texture);
-        //material.SetFloat("_Intensity", intensity);
-        //material.SetFloat("_SourceIntensity", sourceIntensity);
+        material.SetFloat("_NoiseThreshold", noiseThreshold);
+        material.SetFloat("_Intensity", intensity);
 
         Graphics.Blit(source, destination, material);
     }
