eca9a796191892d20a28f114ca15edeb9da46654
samuli <jaaskelainen.samuli@gmail.com>
2018-01-27 13:51:33 +0200

    Made project ready for builds. Added quit and restart. Changed main camera depth to be higher than Vive's.

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
