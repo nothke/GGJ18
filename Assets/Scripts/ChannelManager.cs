85cb96a797b36fb0db75868756ffb3cef3727439
Ivan Notaros <nothke@gmail.com>
2018-01-27 23:32:25 +0200

    Added some more debug options to ChannelDebugger, including channel changing

--- Assets/Scripts/ChannelManager.cs
+++ Assets/Scripts/ChannelManager.cs
@@ -10,6 +10,8 @@ public class ChannelManager : MonoBehaviour
 
     SpotsEffect spots;
 
+    public bool clampBlending = true;
+
     [Header("Debugging only:")]
     public Channel channel1;
     public Channel channel2;
@@ -25,6 +27,11 @@ public class ChannelManager : MonoBehaviour
         // Find all channels in scene
         allChannels = FindObjectsOfType<Channel>();
 
+        for (int i = 0; i < allChannels.Length; i++)
+        {
+            allChannels[i].videoPlayer.Pause();
+        }
+
         spots = FindObjectOfType<SpotsEffect>();
 
         // Init render textures
@@ -38,22 +45,31 @@ public class ChannelManager : MonoBehaviour
 
     public void SwitchChannels(Channel primaryChannel, Channel secondaryChannel)
     {
+        // Pause previous channels
+        if (channel1)
+            channel1.videoPlayer.Pause(); // Move to method in channel
+        if (channel2)
+            channel2.videoPlayer.Pause();
+
+        // assign new
         channel1 = primaryChannel;
         channel2 = secondaryChannel;
-
+        
         channel1.SetOutput(rts[0]);
         channel2.SetOutput(rts[1]);
 
-        // We don't need to change this every time right?
+        // We don't need to change this every time right? (not sure)
         //if (spots.tex1 == null || spots.tex2 == null)
         //{
-        spots.tex1 = rts[0];
-        spots.tex2 = rts[1];
+            spots.tex1 = rts[0];
+            spots.tex2 = rts[1];
         //}
     }
 
     public void SetChannelBlendValue(float blend)
     {
+        if (clampBlending) blend = Mathf.Clamp01(blend);
+
         spots.textureBlend = blend;
 
         channel1.audioSource.volume = 1 - blend;
