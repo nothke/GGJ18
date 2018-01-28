87a70ae980b46f5afd1aeb19986f014801fc95d7
ionutm2004ro <ionut_m2004ro@yahoo.com>
2018-01-28 12:19:04 +0200

    Added ;

--- Assets/Scripts/ChannelManager.cs
+++ Assets/Scripts/ChannelManager.cs
@@ -82,7 +82,8 @@ public class ChannelManager : MonoBehaviour
 
         if (channel1 != null)
         {
-            channel1.audioSource.volume = Mathf.Clamp(1 - blend, 0.001f, 1)
+            channel1.audioSource.volume = Mathf.Clamp(1 - blend, 0.001f, 1);
             channel2.audioSource.volume = Mathf.Clamp(blend, 0.001f, 1);
-        }    }
+        }
+    }
 }
